ffc236a4e8a2a8ef0f93b06e9f6cc8074a824ae4
Kouki Kasuga <146417154+KoukiKasuga@users.noreply.github.com>
2025-11-25 23:59:02 +0100

    Assignment is done.

--- clean_and_scale_scores.py
+++ clean_and_scale_scores.py
@@ -2,5 +2,10 @@ import numpy as np
 
 def clean_and_scale_scores(scores, min_score, max_score):
     ### Replace with your own code (begin) ###
+    scores = scores.copy()
+    scores[scores < min_score] = min_score
+    scores[scores > max_score] = max_score
+    scaled = (scores - min_score) / (max_score - min_score)
+    return scaled
     pass
     ### Replace with your own code (end)   ###
--- count_values_in_bins.py
+++ count_values_in_bins.py
@@ -2,6 +2,20 @@ import numpy as np
 
 def count_values_in_bins(data, bin_edges):
     ### Replace with your own code (begin) ###
+    B = len(bin_edges) - 1
+    counts = np.zeros(B, dtype=int)
+
+    for x in data:
+        for i in range(B):
+            if i == B - 1:
+                if bin_edges[i] <= x <= bin_edges[i + 1]:
+                    counts[i] += 1
+                    break
+            else:
+                if bin_edges[i] <= x < bin_edges[i + 1]:
+                    counts[i] += 1
+                    break
+    return counts
     pass
     ### Replace with your own code (end)   ###
 
